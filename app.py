11c64a42ba22245cc0cfb4b5826cbc6c8d3e7c0d
Hoodoo42 <create@hoodooverse.ca>
2022-10-01 14:17:08 -0600

    working through dbhelpers and writing better code as a simpler more precise way

---
+++ app.py
@@ -0,0 +1,12 @@
+
+import mariadb
+import dbhelpers as dbh
+
+def choose_rooms():
+    user_choice = input('how many rooms ')
+    results = dbh.execute_statement('CALL room_check(?)', [user_choice])
+
+
+def choose_city():
+    city = input('how many rooms ')
+    results = dbh.execute_statement('CALL room_check(?)', [city])
